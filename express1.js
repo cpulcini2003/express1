601d5cdd0bb0ba5d2032b1aa24464f10f6a86558
cpulcini2003 <64283587+cpulcini2003@users.noreply.github.com>
2022-09-22 17:40:38 +0200

    Create express1.js

---
+++ express1.js
@@ -0,0 +1,21 @@
+{
+  "dependencies": {
+    "express": "^4.18.1"
+  }
+}
+pcarlo@pcarlo-a01 cpu-nodejs-api % cat express1.js 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})

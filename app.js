5855ef3601f860d05bcdefda3a050a3fa7981978
cpulcini2003 <64283587+cpulcini2003@users.noreply.github.com>
2022-09-22 23:45:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+app.get('/', function (req, res) {
+  res.send('Hello World')
 })
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+app.listen(3000)
